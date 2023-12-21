868dcf2d86de7beafb9a67b9bbf5373e9d1c4099
thiha-onow <thihaaung@opportunitiesnow.org>
2023-12-22 00:03:56 +0630

    ⚡️ add login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
-from .models import db
-
-bootstrap = Bootstrap()
 app = Flask(__name__)
-db.init_app(app)
-bootstrap.init_app(app)
+
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
